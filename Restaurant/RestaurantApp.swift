37d05a4bff7dbeb9cf74dcd0e5c855914b8aa347
Jack Swindell <jackson1swindell@gmail.com>
2024-03-06 17:20:48 -0600

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Swindell, Jack on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

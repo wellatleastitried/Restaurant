5cef175b5fa57693e3daa3028b80808dd8793aad
Jack Swindell <jackson1swindell@gmail.com>
2024-03-11 15:26:16 -0500

    Progress

---
+++ Restaurant/EditSourceView.swift
@@ -0,0 +1,18 @@
+//
+//  EditSourceView.swift
+//  Restaurant
+//
+//  Created by Swindell, Jack on 3/11/24.
+//
+
+import SwiftUI
+
+struct EditSourceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditSourceView()
+}
